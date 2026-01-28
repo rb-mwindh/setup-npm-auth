995b6fb154853beaa77dd3b156d821e7e8ed1651
rb-mwindh <44254831+rb-mwindh@users.noreply.github.com>
2026-01-28 09:47:03 +0100

    feat(action): allow multiline data for  input

--- src/action-entrypoint.cjs
+++ src/action-entrypoint.cjs
@@ -3,16 +3,29 @@ import { exec } from '@actions/exec';
 
 async function run() {
   try {
-    const include = core.getInput('include');
+    const includes = core.getMultilineInput('includes', { trimWhitespace: true });
     const location = core.getInput('location');
     const dryRun = core.getBooleanInput('dry-run');
     const verbose = core.getBooleanInput('verbose');
 
     let args = [];
-    if (include) args.push('--include', include);
-    if (location) args.push('--location', location);
-    if (dryRun) args.push('--dry-run');
-    if (verbose) args.push('--verbose');
+    for (const inc of includes) {
+      if (inc) {
+        args.push('--include', inc);
+      }
+    }
+
+    if (location) {
+      args.push('--location', location);
+    }
+
+    if (dryRun) {
+      args.push('--dry-run');
+    }
+
+    if (verbose) {
+      args.push('--verbose');
+    }
 
     await exec(`node ./src/setup-npm-auth.mjs`, args);
   } catch (error) {
